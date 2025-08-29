7c2406039aa7a6e369cc65c97940a5a26e19a35a
VoidedPrinter <124522107+VoidedPrinter@users.noreply.github.com>
2025-08-29 17:53:48 -0600

    Loaded questions from a js file and can go from question to question

---
+++ public/js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        number: 1,
+        question: "HTML",
+        answer: "Structure",
+        options: [
+            "Structure",
+            "Styling",
+            "Interative",
+            "Testing"
+        ]
+    },
+    {
+        number: 2,
+        question: "CSS",
+        answer: "Styling",
+        options: [
+            "Interative",
+            "Structure",
+            "Styling",
+            "Testing"
+        ]
+    },
+    {
+        number: 3,
+        question: "JavaScript",
+        answer: "Interative",
+        options: [
+            "Styling",
+            "Structure",
+            "Interative",
+            "Testing"
+        ]
+    },
+    {
+        number: 4,
+        question: "React",
+        answer: "Structure",
+        options: [
+            "Structure",
+            "Interative",
+            "Testing",
+            "Styling"
+        ]
+    },
+    {
+        number: 5,
+        question: "Selenium",
+        answer: "Testing",
+        options: [
+            "Styling",
+            "Testing",
+            "Structure",
+            "Interative"
+        ]
+    }
+]
--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,10 @@ const exitBtn = document.getElementById("exit-btn");
 const main = document.getElementById("main");
 const continueBtn = document.getElementById("continue-btn");
 const quizSection = document.getElementById("quiz-section");
+const quizBox = document.getElementById("quiz-box");
+
+const nextBtn = document.getElementById("next-btn");
+const optionList = document.getElementById("option-list");
 
 function showElement(element) {
     // make element visible by adding the active class
@@ -33,4 +37,31 @@ continueBtn.onclick = () => {
     quizSection.classList.add('active');
     hideElement(popupInfo);
     main.classList.remove('active');
+    quizBox.classList.add('active');
+
+    showQuestions(0);
+}
+
+nextBtn.onclick = () => {
+    if (questionCount < questions.length - 1) {
+        questionCount++;
+        showQuestions(questionCount);
+    } else {
+        console.log("Quiz Completed!");
+    }
+}
+
+// Quiz
+let questionCount = 0;
+
+function showQuestions(index) {
+    const questionText = document.getElementById("question-text");
+    questionText.textContent = `${questions[index].number}. ${questions[index].question}`;
+
+    const option1 = document.getElementById("option-1");
+    option1.textContent = `${questions[index].options[0]}`;
+
+    document.getElementById("option-2").textContent = `${questions[index].options[1]}`;
+    document.getElementById("option-3").textContent = `${questions[index].options[2]}`;
+    document.getElementById("option-4").textContent = `${questions[index].options[3]}`;
 }
